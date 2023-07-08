bc8bcd33e0f60f4ae086183dc38a3657d75468df
jacob <jacobbishopxy@gmail.com>
2023-07-08 22:46:13 +0800

    :hatching_chick: Ch04

---
+++ tests/syntax_in_fns.hs
@@ -0,0 +1,133 @@
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+-- (x:[])
+tell [x] = "The list has one element: " ++ show x
+-- (x:y:[])
+tell [x, y] = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x : y : _) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_ : xs) = 1 + length' xs
+
+-- the code below will be optimized into `sum`
+-- sum' :: (Num a) => [a] -> a
+-- sum' [] = 0
+-- sum' (x : xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(x : xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+  | bmi <= 18.5 = "You're underweight, you emo, you!"
+  | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+
+bmiTell' :: (RealFloat a) => a -> a -> String
+bmiTell' weight height
+  | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+  | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a > b = a
+  | otherwise = b
+
+compare' :: (Ord a) => a -> a -> Ordering
+a `compare'` b
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
+
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' weight height
+  | bmi <= 18.5 = "You're underweight, you emo, you!"
+  | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where
+    bmi = weight / height ^ 2
+
+bmiTell''' :: (RealFloat a) => a -> a -> String
+bmiTell''' weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where
+    bmi = weight / height ^ 2
+    skinny = 18.5
+    normal = 25.0
+    fat = 30.0
+
+initials :: String -> String -> String
+initials first_name last_name = [f] ++ ". " ++ [l] ++ "."
+  where
+    (f : _) = first_name
+    (l : _) = last_name
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where
+    bmi weight height = weight / height ^ 2
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+calcBmis'' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis'' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+-- Case expressions
+
+head' :: [a] -> a
+head' [] = error "No head for empty lists!"
+head' (x : _) = x
+
+head'' :: [a] -> a
+head'' xs = case xs of
+  [] -> error "No head for empty lists!"
+  (x : _) -> x
+
+describeList :: [a] -> String
+describeList xs =
+  "The list is " ++ case xs of
+    [] -> "empty."
+    [x] -> "a singleton list."
+    xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+  where
+    what [] = "empty."
+    what [x] = "a singleton list."
+    what xs = "a longer list."
