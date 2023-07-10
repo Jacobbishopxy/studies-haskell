af0c1fe8f783d4d7af2f2b5f1dcf57c6228de2cb
jacob-66.54 <jacobbishopxy@gmail.com>
2023-07-10 17:30:50 +0800

    :construction: Ch06

--- learn_me_a_haskell/code/higher_order_fns.hs
+++ learn_me_a_haskell/code/higher_order_fns.hs
@@ -20,3 +20,42 @@ filp' f = g
 --
 filp'' :: (a -> b -> c) -> b -> a -> c
 filp'' f y x = f x y
+
+--
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x : xs) = f x : map' f xs
+
+--
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x : xs)
+  | p x = x : filter' p xs
+  | otherwise = filter' p xs
+
+--
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let smallerSorted = quicksort (filter (<= x) xs)
+      biggerSorted = quicksort (filter (> x) xs)
+   in smallerSorted ++ [x] ++ biggerSorted
+
+--
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999 ..])
+  where
+    p x = x `mod` 3829 == 0
+
+-- Collatz sequences
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n * 3 + 1)
+
+--
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1 .. 100]))
+  where
+    isLong xs = length xs > 15
