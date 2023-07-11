2d280a25e7b7d86fa985ca0ff2e1dc5ee2717f37
jacob-66.54 <jacobbishopxy@gmail.com>
2023-07-11 13:46:10 +0800

    :construction: Ch06

--- learn_me_a_haskell/code/higher_order_fns.hs
+++ learn_me_a_haskell/code/higher_order_fns.hs
@@ -71,3 +71,31 @@ numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1 .. 100]))
 -- comment this because it would be optimized
 -- elem' :: (Eq a) => a -> [a] -> Bool
 -- elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+--
+-- maximum' :: (Ord a) => [a] -> a
+-- maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+--
+-- reverse' :: [a] -> [a]
+-- reverse' = foldl (\acc x -> x : acc) []
+
+--
+-- product' :: (Num a) => [a] -> a
+-- product' = foldr1 (*)
+
+--
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+--
+-- head' :: [a] -> a
+-- head' = foldr1 (\x _ -> x)
+
+--
+-- last' :: [a] -> a
+-- last' = foldl1 (\_ x -> x)
+
+--
+sqrtSums :: Int
+sqrtSums = length (takeWhile (< 1000) (scanl1 (+) (map sqrt [1 ..]))) + 1
