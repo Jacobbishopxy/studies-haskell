410122146626d2b45b4b229f100377d24a1b2589
jacob <jacobbishopxy@gmail.com>
2023-07-14 22:57:36 +0800

    :construction: Ch07

--- learn_me_a_haskell/code/modules.hs
+++ learn_me_a_haskell/code/modules.hs
@@ -3,7 +3,8 @@
 -- date: 2023/07/12 09:02:04 Wednesday
 -- brief:
 
-import Data.List
+import Data.List (nub, tails)
+import Data.Map qualified as Map
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -15,3 +16,12 @@ search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack =
   let nlen = length needle
    in foldl (\acc x -> (take nlen x == needle) || acc) False (tails haystack)
+
+fromList' :: (Ord k) => [(k, v)] -> Map.Map k v
+fromList' = foldr (\(k, v) acc -> Map.insert k v acc) Map.empty
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2)
+
+phoneBookToMap' :: (Ord k) => [(k, a)] -> Map.Map k [a]
+phoneBookToMap' = Map.fromListWith (++) . map (\(k, v) -> (k, [v]))
