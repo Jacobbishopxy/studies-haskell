904da8903d009003b186a8028240b2906bba1bb6
jacob <jacobbishopxy@gmail.com>
2023-07-16 23:37:05 +0800

    :construction: Ch08

--- learn_me_a_haskell/code/own_types2.hs
+++ learn_me_a_haskell/code/own_types2.hs
@@ -30,3 +30,52 @@ lockers =
       (109, (Taken, "893JJ")),
       (110, (Taken, "99292"))
     ]
+
+--
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+-- Same as `Cons`
+infixr 5 :-:
+
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+-- Same as `++`
+infixr 5 .++
+
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+-- Binary Search Tree
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
+
+-- Typeclasses 102
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Yellow = "Yellow light"
+  show Green = "Green light"
