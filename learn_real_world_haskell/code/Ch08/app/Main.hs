c05b16e61eed4d252c4f0a0050dc3ce349114255
jacob <jacobbishopxy@gmail.com>
2023-10-14 19:55:27 +0800

    :construction: `Ch9`

--- learn_real_world_haskell/code/Ch08/app/Main.hs
+++
@@ -1,8 +0,0 @@
--- file: Main.hs
--- author: Jacob Xie
--- date: 2023/10/12 00:15:52 Thursday
--- brief:
-
-main :: IO ()
-main = do
-  putStrLn "whatever"
---
+++ learn_real_world_haskell/code/Ch09/RecursiveContents.hs
@@ -0,0 +1,21 @@
+-- file: RecursiveContents.hs
+-- author: Jacob Xie
+-- date: 2023/10/14 19:44:01 Saturday
+-- brief:
+
+module RecursiveContents (getRecursiveContents) where
+
+import Control.Monad (forM)
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath ((</>))
+
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", ".."]) names
+  paths <- forM properNames $ \name -> do
+    let path = topdir </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getRecursiveContents path
+      else return [path]
+  return (concat paths)
