62e7aa356d233486a53d322e344eb8edacf9d7de
jacob <jacobbishopxy@gmail.com>
2023-10-21 20:37:52 +0800

    :construction: Ch9

--- learn_real_world_haskell/code/Ch09/BetterPredicate.hs
+++ learn_real_world_haskell/code/Ch09/BetterPredicate.hs
@@ -9,8 +9,8 @@ import Data.Time.Clock (UTCTime)
 import GHC.IO.Handle.FD (withFile)
 import RecursiveContents (getRecursiveContents)
 import System.Directory (Permissions (..), getModificationTime, getPermissions)
-import System.FilePath (takeExtensions)
-import System.IO (IOMode (..), hClose, hFileSize, openFile)
+import System.FilePath (takeExtension, takeExtensions)
+import System.IO (IOMode (ReadMode), hClose, hFileSize, openFile)
 
 type Predicate =
   FilePath ->
@@ -54,9 +54,97 @@ saferFileSize path = handle errorHandler $ do
 getFileSize :: FilePath -> IO (Maybe Integer)
 getFileSize path = handle errorHandler $
   -- bracket (openFile path ReadMode) hClose $ \h -> do
+
+  -- bracket (openFile path ReadMode) hClose $ \h -> do
   withFile path ReadMode $ \h -> do
     size <- hFileSize h
     return (Just size)
   where
     errorHandler :: SomeException -> IO (Maybe Integer)
     errorHandler _ = return Nothing
+
+-- ////////////////////////////////////////////////////////////////////////////////////////////////
+-- embedded domain specific language
+
+myTest :: (Ord a, Num a) => FilePath -> p1 -> Maybe a -> p2 -> Bool
+myTest path _ (Just size) _ = takeExtension path == ".cpp" && size > 131072
+myTest _ _ _ _ = False
+
+-- explicit type announcing
+type InfoP a =
+  FilePath -> -- path to directory entry
+  Permissions -> -- permissions
+  Maybe Integer -> -- file size (Nothing if not file)
+  UTCTime -> -- last modified
+  a
+
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
+
+--
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing _ = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+-- equalP f k = \w x y z -> f w x y z == k
+equalP f k w x y z = f w x y z == k
+
+-- equalP' :: (Eq a) => InfoP a -> a -> InfoP Bool
+-- equalP' f k w x y z = f w x y z == k
+
+-- avoid boilerplate w/ lifting
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP = liftP (<)
+
+-- gluing predicates together
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g w x y z = f w x y z && g w x y z
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+andP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+andP = liftP2 (&&)
+
+orP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+orP = liftP2 (||)
+
+constP :: a -> InfoP a
+constP k _ _ _ _ = k
+
+liftP' q f k w x y z = f w x y z `q` constP k w x y z
+
+-- myTest2, use combinator
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath f w _ _ _ = f w
+
+myTest2 = (liftPath takeExtension `equalP` ".cpp") `andP` (sizeP `greaterP` 131072)
+
+-- infix operators
+
+-- explicit annotation
+(==?) :: InfoP String -> String -> InfoP Bool
+(==?) = equalP
+
+(&&?) :: InfoP Bool -> InfoP Bool -> InfoP Bool
+(&&?) = andP
+
+-- explicit annotation
+(>?) :: InfoP Integer -> Integer -> InfoP Bool
+(>?) = greaterP
+
+myTest3 = (liftPath takeExtension ==? ".cpp") &&? (sizeP >? 131072)
+
+-- parenthesis-free expression
+infix 4 ==?
+
+infixr 3 &&?
+
+infix 4 >?
+
+myTest4 = liftPath takeExtension ==? ".cpp" &&? sizeP >? 131072
