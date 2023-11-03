bdcdc406b101cdcd2b7fccd85dd74bae0b1eab9c
jacob-66.54 <jacobbishopxy@gmail.com>
2023-11-03 14:41:57 +0800

    :construction: Ch10

---
+++ learn_real_world_haskell/code/Ch10/PNM.hs
@@ -0,0 +1,66 @@
+-- file: PNM.hs
+-- author: Jacob Xie
+-- date: 2023/11/02 14:41:10 Thursday
+-- brief:
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Char (isSpace)
+
+data Greymap = Greymap
+  { greyWidth :: Int,
+    greyHeight :: Int,
+    greyMax :: Int,
+    greyData :: L.ByteString
+  }
+  deriving (Eq)
+
+instance Show Greymap where
+  show (Greymap w h m _) = "Greymap " ++ show w ++ "x" ++ show h ++ " " ++ show m
+
+-- header
+matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
+matchHeader prefix str
+  | prefix `L8.isPrefixOf` str = Just (L8.dropWhile isSpace (L.drop (L.length prefix) str))
+  | otherwise = Nothing
+
+-- nat = natural number
+getNat :: L.ByteString -> Maybe (Int, L.ByteString)
+getNat s = case L8.readInt s of
+  Nothing -> Nothing
+  Just (num, rest)
+    | num <= 0 -> Nothing
+    | otherwise -> Just (fromIntegral num, rest)
+
+getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
+getBytes n str =
+  let count = fromIntegral n
+      both@(prefix, _) = L.splitAt count str
+   in if L.length prefix < count
+        then Nothing
+        else Just both
+
+-- parse function
+parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
+parseP5 s =
+  case matchHeader (L8.pack "PS") s of
+    Nothing -> Nothing
+    Just s1 ->
+      case getNat s1 of
+        Nothing -> Nothing
+        Just (width, s2) ->
+          case getNat (L8.dropWhile isSpace s2) of
+            Nothing -> Nothing
+            Just (height, s3) ->
+              case getNat (L8.dropWhile isSpace s3) of
+                Nothing -> Nothing
+                Just (maxGrey, s4)
+                  | maxGrey > 255 -> Nothing
+                  | otherwise ->
+                      case getBytes 1 s4 of
+                        Nothing -> Nothing
+                        Just (_, s5) ->
+                          case getBytes (width * height) s5 of
+                            Nothing -> Nothing
+                            Just (bitmap, s6) ->
+                              Just (Greymap width height maxGrey bitmap, s6)
