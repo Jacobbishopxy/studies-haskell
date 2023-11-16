134c45368c0ce4308c3b856bcd8657ccf696c7cb
jacob-66.54 <jacobbishopxy@gmail.com>
2023-11-16 17:01:22 +0800

    :construction: Ch11

---
+++ learn_real_world_haskell/code/Ch11/QC-basics.hs
@@ -0,0 +1,8 @@
+-- file: QC-basics.hs
+-- author: Jacob Xie
+-- date: 2023/11/14 20:50:22 Tuesday
+--
+-- deps installation: `cabal install --lib QuickCheck`;
+
+import Data.List
+import Test.QuickCheck
