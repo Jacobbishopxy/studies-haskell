aaa8debf93db24e195f10717ea4ae2ad34269e7f
jacob <jacobbishopxy@gmail.com>
2024-01-11 23:37:13 +0800

    :construction: Ch13

--- learn_real_world_haskell/code/Ch13/num.hs
+++ learn_real_world_haskell/code/Ch13/num.hs
@@ -116,3 +116,137 @@ simpleParen x@(UnaryArith {}) = prettyShow x
 -}
 instance (Show a, Num a) => Show (SymbolicManips a) where
   show = prettyShow
+
+{-
+  Show a SymbolicManip using RPN. HP calculator users may find this familiar.
+-}
+rnpShow :: (Show a, Num a) => SymbolicManips a -> String
+rnpShow i =
+  let toList (Number x) = [show x]
+      toList (Symbol x) = [x]
+      toList (BinaryArith op a b) = toList a ++ toList b ++ [op2str op]
+      toList (UnaryArith op a) = toList a ++ [op]
+      join :: [a] -> [[a]] -> [a]
+      join = intercalate
+   in join " " (toList i)
+
+{-
+  Perform some basic algebraic simplifications on a SymbolicManip
+-}
+simplify :: (Num a, Eq a) => SymbolicManips a -> SymbolicManips a
+simplify (BinaryArith op ia ib) =
+  let sa = simplify ia
+      sb = simplify ib
+   in case (op, sa, sb) of
+        (Mul, Number 1, b) -> b
+        (Mul, a, Number 1) -> a
+        (Mul, Number 0, _) -> Number 0
+        (Mul, _, Number 0) -> Number 0
+        (Div, a, Number 1) -> a
+        (Plus, a, Number 0) -> a
+        (Plus, Number 0, b) -> b
+        (Minus, a, Number 0) -> a
+        _ -> BinaryArith op sa sb
+simplify (UnaryArith op a) = UnaryArith op (simplify a)
+simplify x = x
+
+{-
+  New data type: Units.
+  A Units type contains a number and a SymbolicManip, which represents the units of measure.
+  A simple label would be something like (Symbol "m")
+-}
+data Units a = Units a (SymbolicManips a) deriving (Eq)
+
+{-
+  Implement Units for Num.
+  We don't know how to convert between arbitrary units, so we generate an error if we try to add
+  numbers with different units. For multiplication, generate the appropriate new units.
+-}
+instance (Num a, Eq a) => Num (Units a) where
+  (Units xa ua) + (Units xb ub)
+    | ua == ub = Units (xa + xb) ua
+    | otherwise = error "Mis-matched units in add or subtract"
+  (Units xa ua) - (Units xb ub) = Units xa ua + Units (xb * (-1)) ub
+  (Units xa ua) * (Units xb ub) = Units (xa * xb) (ua * ub)
+  negate (Units xa ua) = Units (negate xa) ua
+  abs (Units xa ua) = Units (abs xa) ua
+  signum (Units xa _) = Units (signum xa) (Number 1)
+  fromInteger i = Units (fromInteger i) (Number 1)
+
+{-
+  Make Units an instance of Fractional
+-}
+instance (Fractional a, Eq a) => Fractional (Units a) where
+  (Units xa ua) / (Units xb ub) = Units (xa / xb) (ua / ub)
+  recip a = 1 / a
+  fromRational r = Units (fromRational r) (Number 1)
+
+{-
+  Floating implementation for Units.
+  Use some intelligence for angle calculations: support deg and rad
+-}
+instance (Floating a, Eq a) => Floating (Units a) where
+  pi = Units pi $ Number 1
+  exp = error "exp not yet implemented in Units"
+  log = error "log not yet implemented in Units"
+  sqrt (Units xa ua) = Units (sqrt xa) (sqrt ua)
+  (Units xa ua) ** (Units xb ub)
+    | ub == Number 1 = Units (xa ** xb) (ua ** Number xb)
+    | otherwise = error "units for RHS of ** not supported"
+  logBase _ = error "logBase not yet implemented in Units"
+  sin (Units xa ua)
+    | ua == Symbol "rad" = Units (sin xa) (Number 1)
+    | ua == Symbol "deg" = Units (sin (deg2rad xa)) (Number 1)
+    | otherwise = error "Units for sin must be empty"
+  cos (Units xa ua)
+    | ua == Symbol "rad" = Units (cos xa) (Number 1)
+    | ua == Symbol "deg" = Units (cos (deg2rad xa)) (Number 1)
+    | otherwise = error "Units for cos must be empty"
+  tan (Units xa ua)
+    | ua == Symbol "rad" = Units (tan xa) (Number 1)
+    | ua == Symbol "deg" = Units (tan (deg2rad xa)) (Number 1)
+    | otherwise = error "Units for tan must be empty"
+  asin (Units xa ua)
+    | ua == Number 1 = Units (rad2deg $ asin xa) (Symbol "deg")
+    | otherwise = error "Units for asin must be empty"
+  acos (Units xa ua)
+    | ua == Number 1 = Units (rad2deg $ acos xa) (Symbol "deg")
+    | otherwise = error "Units for acos must be empty"
+  atan (Units xa ua)
+    | ua == Number 1 = Units (rad2deg $ atan xa) (Symbol "deg")
+    | otherwise = error "Units for atan must be empty"
+  sinh = error "sinh not yet implemented in Units"
+  cosh = error "cosh not yet implemented in Units"
+  tanh = error "tanh not yet implemented in Units"
+  asinh = error "asinh not yet implemented in Units"
+  acosh = error "acosh not yet implemented in Units"
+  atanh = error "atanh not yet implemented in Units"
+
+{-
+  A simple function that takes a number and a String and returns an appropriate Units type to
+  represent the number and its unit of measure
+-}
+units :: (Num z) => z -> String -> Units z
+units a b = Units a $ Symbol b
+
+{-
+  Extract the number only out of a Units type
+-}
+dropUnits :: (Num z) => Units z -> z
+dropUnits (Units x _) = x
+
+{-
+  Utilities for the Unit implementation
+-}
+deg2rad x = 2 * pi * x / 360
+
+rad2deg x = 360 * x / (2 * pi)
+
+{-
+  Showing units: we show the numeric component, an underscore, then the prettyShow version of the simplified units
+-}
+instance (Show a, Num a, Eq a) => Show (Units a) where
+  show (Units xa ua) = show xa ++ "_" ++ prettyShow (simplify ua)
+
+test :: (Num a) => a
+test = 2 * 5 + 3
